93973d797b04cf87168e87b6ffbdce4b9c574b10
araortiz99 <andrea.ortizab@gmail.com>
2025-02-16 14:17:06 -0600

    araortiz99

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/araortiz99.py
@@ -0,0 +1,42 @@
+
+#https://python.org
+
+#Comentario de una sola línea
+
+"""
+Comentario de 
+varias líneas
+"""
+
+my_variable = "Mi Variable"
+my_variable = "Nuevo valor de variable"
+
+MY_CONSTANT = "Mi constante" #Por convención, las constantes se escriben en mayúsculas
+
+
+#Tipos de datos Primitivos
+my_int : int = 1
+my_float = 1.0
+my_str = "Hola Python"
+my_bool = False
+my_bool = True
+my_list = ["a", "b", "c"]
+my_tuple = ("a", "b", "c")
+my_dict = {"key": "value"}
+my_set = {"a", "b", "c"}
+
+
+
+
+print(my_variable)
+print(type(MY_CONSTANT))
+print(type(my_int))
+print(type(my_float))
+print(type(my_str))
+print(type(my_bool))
+print(type(my_list))
+print(type(my_tuple))
+print(type(my_dict))
+print(type(my_set))
+
+
